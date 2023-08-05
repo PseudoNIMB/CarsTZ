d70c10e4353bd598eff4c0f03ac8c37b4d0e1ac6
PseudoNIMB <ienndevld@yandex.ru>
2023-08-06 00:26:34 +0300

    pre-final build, добавление записи в бд.

--- app/src/main/java/com/example/carstz/Car.kt
+++ app/src/main/java/com/example/carstz/Car.kt
@@ -2,4 +2,10 @@ package com.example.carstz
 
 import java.io.Serializable
 
-data class Car(val imageId: Int, val title: String, val desc: String): Serializable
+data class Car(val imageId: Int,
+               val title: String,
+               val desc: String,
+               val engine: String,
+               val trans: String,
+               val price: String
+): Serializable
--- app/src/main/java/com/example/carstz/Item.kt
+++ app/src/main/java/com/example/carstz/Item.kt
@@ -6,18 +6,10 @@ import androidx.room.PrimaryKey
 
 @Entity (tableName = "car.db")
 data class Item (
-    @PrimaryKey(autoGenerate = true)
-    var id: Int? = null,
-    @ColumnInfo(name = "brand")
-    var brand: String,
-    @ColumnInfo(name = "model")
-    var model: String,
-    @ColumnInfo(name = "engine")
-    var engine: String,
-    @ColumnInfo(name = "transmission")
-    var transmission: String,
-    @ColumnInfo(name = "price")
-    var price: String,
-
-
+    @PrimaryKey(autoGenerate = true)    var id: Int? = null,
+    @ColumnInfo(name = "brand")    var brand: String,
+    @ColumnInfo(name = "model")    var model: String,
+    @ColumnInfo(name = "engine")    var engine: String,
+    @ColumnInfo(name = "transmission")    var transmission: String,
+    @ColumnInfo(name = "price")    var price: String
 )
