c83b264058af9c55d9ad7e76b909ce81c6a7d5ae
PseudoNIMB <ienndevld@yandex.ru>
2023-08-05 18:45:17 +0300

    Завершение работы с вёрсткой, переход к адаптеру.

---
+++ app/src/main/java/com/example/carstz/CarAdapter.kt
@@ -0,0 +1,13 @@
+package com.example.carstz
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class CarAdapter : RecyclerView.Adapter<CarAdapter.CarHolder> {
+
+    class CarHolder(item: View) : RecyclerView.ViewHolder(item) {
+        fun bind(car: Car){
+
+        }
+    }
+}
