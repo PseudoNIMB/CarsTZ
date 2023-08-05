3a501587d0df2249c087575e5fa6205a5530c7f8
PseudoNIMB <ienndevld@yandex.ru>
2023-08-05 18:59:07 +0300

    Окончание работы с адаптером

--- app/src/main/java/com/example/carstz/CarAdapter.kt
+++ app/src/main/java/com/example/carstz/CarAdapter.kt
@@ -1,13 +1,31 @@
 package com.example.carstz
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.carstz.databinding.CarItemBinding
 
-class CarAdapter : RecyclerView.Adapter<CarAdapter.CarHolder> {
-
+class CarAdapter : RecyclerView.Adapter<CarAdapter.CarHolder>() {
+    val carList = ArrayList<Car>()
     class CarHolder(item: View) : RecyclerView.ViewHolder(item) {
-        fun bind(car: Car){
-
+        val binding = CarItemBinding.bind(item)
+        fun bind(car: Car) = with(binding){
+            im.setImageResource(car.imageId)
+            tvTitle.text = car.title
         }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.car_item, parent, false)
+        return CarHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return carList.size
+    }
+
+    override fun onBindViewHolder(holder: CarHolder, position: Int) {
+        holder.bind(carList[position])
     }
 }
